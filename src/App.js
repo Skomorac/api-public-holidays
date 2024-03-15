54ad9c7d7a266f8fedbe1dd6b70bfae6a77ecc39
Skomorac <admir.skomorac@outlook.com>
2024-03-15 20:59:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [holidays, setHolidays] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://date.nager.at/api/v3/publicholidays/2024/BA"
+        );
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        setHolidays(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Public holidays in Bosnia and Herzegovina</h1>
+      {holidays ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Local Name</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {holidays.map((holiday, index) => (
+              <tr key={index}>
+                <td>{holiday.date}</td>
+                <td>{holiday.localName}</td>
+                <td>{holiday.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
